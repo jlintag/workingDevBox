b455f17d75e432852f282ba91f1f6edd598868a2
Joshua Lintag <jlintag@labattfood.com>
2017-04-06 10:49:02 -0500

    Added packer related stuff

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Packer templates based on:https://github.com/kaorimatz/packer-templates
+Shell script based on: https://decomputed.com/posts/2016-03-12-archVagrant.html
---
+++ scripts/archlinux/virtualbox.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+set -e
+set -x
+
+if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ]; then
+  exit 0
+fi
+
+sudo pacman -S --noconfirm linux-headers
+sudo pacman -S --noconfirm virtualbox-guest-utils-nox
+sudo systemctl enable vboxservice
